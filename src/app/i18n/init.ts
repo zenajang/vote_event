8bec3c6ad28c6f383b76a30d633a6c4efd763df4
JungEun-Jang <zenaj@gmeremit.com>
2025-09-09 17:39:57 +0900

    i18next설치

--- src/app/i18n/init.ts
+++ src/app/i18n/init.ts
@@ -1,5 +1,5 @@
 import i18next from 'i18next';
-import { initReactI18next } from 'react-i18next/initReactI18next';
+import { initReactI18next } from 'react-i18next';
 import { i18nConfig } from './settings';
 import { addBackend, namespaces } from './locales';
 
