bdd7cdfd76bd6c3aa3f3b20a78ea723198f4127c
JungEun-Jang <zenaj@gmeremit.com>
2025-09-25 13:52:02 +0900

    에러해결

--- src/app/[locale]/main/page.tsx
+++ src/app/[locale]/main/page.tsx
@@ -1,8 +1,12 @@
+import { Suspense } from "react";
 import MainClient from "./MainClient";
 
 type Props = { params: Promise<{ locale: string }> };
 
 export default async function Page({ params }: Props) {
   const { locale } = await params;
-  return <MainClient locale={locale} />;
-}
+  return (
+  <Suspense fallback={null} >
+    <MainClient locale={locale} />;
+  </Suspense>
+)}
